b9c0db9100189943333b5a216ef04b3ad05d1b83
ViviStan69 <kevinohl@msn.com>
2023-02-21 19:41:24 +0100

    Improve variable naming and declaration

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,12 +4,12 @@ const sumAll = function(num1, num2) {
     if (!(typeof(num1) == "number") || !(typeof(num2) == "number")) return "ERROR";
     // ensure numbers are ordered low to high
     if (num1 > num2) {
-        placeholder = num1;
+        const tmp = num1;
         num1 = num2;
-        num2 = placeholder;
+        num2 = tmp;
     }
-    
-    sum = 0;
+
+    let sum = 0;
     for (let i=num1; i <= num2; i++) {
         sum += i;
     }
