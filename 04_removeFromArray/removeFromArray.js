c732e3363991d3130bbaf69143aa7cb996212cea
ViviStan69 <kevinohl@msn.com>
2023-02-21 19:10:03 +0100

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...elementsToRemove) {
+    resultArray = [];
+    for (const element of array) {
+        if (elementsToRemove.indexOf(element) != -1) {
+            continue;
+        }
+        else resultArray.push(element);
+    }
+    return resultArray;
 };
 
 // Do not edit below this line
