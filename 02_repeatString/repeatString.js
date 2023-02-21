97336d06734eb1643b265a9226d0ec4657537278
ViviStan69 <kevinohl@msn.com>
2023-02-21 16:02:34 +0100

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    outputString = "";
 
+    if (num < 0) return "ERROR";
+
+    for (i=num; i>0; i--) {
+        outputString += string;
+    }
+    
+    return outputString;
 };
 
 // Do not edit below this line
