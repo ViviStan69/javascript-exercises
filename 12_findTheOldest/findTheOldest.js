847b55d1c81f45f816327c62ea2b0aebb47b5751
Kevin Ohl <kevinohl@msn.com>
2023-02-25 15:42:12 +0100

    Solve exercises 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const peopleWithAges = people.map(person => person);
+    peopleWithAges.map(person => {
+        if (!person.yearOfDeath) person["yearOfDeath"] = [new Date().getFullYear()];
+        person["age"] = person.yearOfDeath - person.yearOfBirth;
+    })
+    const maxAge = Math.max(... peopleWithAges.map(person => +person.age));
+    oldestPersonIndex = peopleWithAges.findIndex(person => +person.age == +maxAge);
+    return people[oldestPersonIndex];
+    //.sort((person1, person2) => person1.age - person2.age);
 };
 
 // Do not edit below this line
